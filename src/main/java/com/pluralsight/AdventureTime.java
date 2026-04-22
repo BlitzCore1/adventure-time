a9b852e4f2c7c35ff4eb16412e29ccb589a1ae31
damarriusthompson <dthompson1@my.yearupunited.org>
2026-04-22 11:24:13 -0700

    Load csv data

--- src/main/java/com/pluralsight/AdventureTime.java
+++ src/main/java/com/pluralsight/AdventureTime.java
@@ -1,7 +1,38 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
 public class AdventureTime {
-    public static void main(){
+    public static void main()
+    {
+        loadAdventure();
+    }
+
+
+
+    public static void loadAdventure()
+    {
+        try
+        {
+            FileReader fileReader = new FileReader("adventure1.csv");
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line = bufferedReader.readLine();
+
+            while (line != null)
+            {
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+
+
+        }
+        catch (Exception ex)
+        {
 
     }
+}
 }

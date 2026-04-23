e19f287b972c5cdc6584aaba860884f9214ca5f3
damarriusthompson <dthompson1@my.yearupunited.org>
2026-04-23 15:23:56 -0700

    completed functionality of AdventureTime

--- src/main/java/com/pluralsight/AdventureTime.java
+++ src/main/java/com/pluralsight/AdventureTime.java
@@ -9,14 +9,16 @@ import java.util.Scanner;
 public class AdventureTime
 {
     static ArrayList<StoryStep> adventureSteps;
+    Scanner userInput = new Scanner(System.in);
+
     public static void main()
     {
         adventureSteps = loadAdventure();
         homeScreen();
     }
 
-    public static void homeScreen(){
-
+    public static void homeScreen()
+    {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to Adventure Time");
@@ -28,30 +30,67 @@ public class AdventureTime
         if (choice.equals("P"))
         {
             gameScreen(1);
-
         }
     }
-
     public static void gameScreen(int id)
+    {
+        int nextId = id;
+
+        while(nextId != -1)
+        {
+            StoryStep step = findStep(nextId);
+
+            Scanner scanner = new Scanner(System.in);
+
+            if(step == null)
+            {
+                System.out.println();
+                System.out.println("An Error has occurred. the step was not found.");
+
+            }
+            else
+            {
+                System.out.println("Story Text: " + step.getStoryText());
+                System.out.println();
+                System.out.println("1) " + step.getOption1Text());
+                System.out.println();
+                System.out.println("2) " + step.getOption2Text());
+                System.out.println("Make a selection");
+
+                String userInput = scanner.nextLine();
+
+                switch(userInput)
+                {
+                    case "1":
+                        nextId = step.getOption1NextId();
+                        System.out.println(step.getOption1NextId());
+                        break;
+
+                    case "2":
+                        nextId = step.getOption2NextId();
+                        System.out.println(step.getOption2NextId());
+                        break;
+
+                    // case
+                }
+            }
+        }
+
+
+
+    }
+    public static StoryStep findStep(int id)
     {
         for (int i = 0; i < adventureSteps.size() ; i++)
         {
             StoryStep step = adventureSteps.get(i);
             if(step.getId() == id)
             {
-                System.out.println("Story Text: " + step.getId());
-                System.out.println();
-                System.out.println("1) " + step.getOption1Text());
-                System.out.println();
-                System.out.println("2) " + step.getOption2Text());
-
+                return step;
             }
-
         }
-
+        return null;
     }
-
-
 
     public static ArrayList<StoryStep> loadAdventure() {
         // create the container
@@ -85,11 +124,9 @@ public class AdventureTime
 
                 line = bufferedReader.readLine();
             }
-
             bufferedReader.close();
-
-
-        } catch (Exception ex)
+        }
+        catch (Exception ex)
         {
 
         }
